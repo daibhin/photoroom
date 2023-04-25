b7016d99fceceeb2ae0400bb5e2c37707d2c4e25
David Newell <david@unflow.com>
2023-04-25 10:42:17 +0100

    Support multiple images

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,43 @@
-import React, { ChangeEvent, useState } from 'react';
-import './App.css';
-import AddButton from './components/AddButton';
-import loadImage, { LoadImageResult } from 'blueimp-load-image';
-import { API_KEY, API_URL, BASE64_IMAGE_HEADER } from './Constants';
+import React, { ChangeEvent, useState } from "react";
+import "./App.css";
+import AddButton from "./components/AddButton";
+import loadImage, { LoadImageResult } from "blueimp-load-image";
+import { API_KEY, API_URL, BASE64_IMAGE_HEADER } from "./Constants";
+
+type Base64Image = string | undefined;
+
+type Image = { original: Base64Image; result: Base64Image };
 
 function App() {
-  const [result, setResult] = useState<string | null>(null)
-  
+  const [images, setImages] = useState<Image[]>([]);
+
   let uploadImageToServer = (file: File) => {
-    loadImage(
-      file,
-      {
-        maxWidth: 400,
-        maxHeight: 400,
-        canvas: true
-      })
+    loadImage(file, {
+      maxWidth: 400,
+      maxHeight: 400,
+      canvas: true,
+    })
       .then(async (imageData: LoadImageResult) => {
-        let image = imageData.image as HTMLCanvasElement
-        
-        let imageBase64 = image.toDataURL("image/png")
-        let imageBase64Data = imageBase64.replace(BASE64_IMAGE_HEADER, "")
+        let localImage = {} as Image;
+
+        let image = imageData.image as HTMLCanvasElement;
+
+        let imageBase64 = image.toDataURL("image/png");
+
+        localImage.original = imageBase64;
+
+        let imageBase64Data = imageBase64.replace(BASE64_IMAGE_HEADER, "");
         let data = {
           image_file_b64: imageBase64Data,
-        }
+        };
         const response = await fetch(API_URL, {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'x-api-key': API_KEY
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            "x-api-key": API_KEY,
           },
-          body: JSON.stringify(data)
+          body: JSON.stringify(data),
         });
 
         if (response.status >= 400 && response.status < 600) {
@@ -38,32 +45,48 @@ function App() {
         }
 
         const result = await response.json();
-        const base64Result = BASE64_IMAGE_HEADER + result.result_b64
-        setResult(base64Result)
+        const base64Result = BASE64_IMAGE_HEADER + result.result_b64;
+
+        localImage.result = base64Result;
+
+        let newImages = [...images];
+        newImages.push(localImage);
+
+        setImages(newImages);
       })
-      
-      .catch(error => {
-        console.error(error)
-      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  let onImageAdd = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      uploadImageToServer(e.target.files[0]);
+    } else {
+      console.error("No file was picked");
     }
-    
-    let onImageAdd = (e: ChangeEvent<HTMLInputElement>) => {
-      if (e.target.files && e.target.files[0]) {
-        uploadImageToServer(e.target.files[0])
-      } else {
-        console.error("No file was picked")
-      }
-    }
-    
-    return (
-      <div className="App">
-        <header className="App-header">
-          {!result && <AddButton onImageAdd={onImageAdd}/>}
-          {result && <img src={result} width={300} alt="result from the API"/>}
-        </header>
-      </div>
-      );
-    }
-    
-    export default App;
-    
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AddButton onImageAdd={onImageAdd} />
+
+        {images.map((image) => {
+          return (
+            <div>
+              <img
+                src={image.original}
+                width={300}
+                alt="original file uploaded"
+              />
+              <img src={image.result} width={300} alt="result from the API" />
+            </div>
+          );
+        })}
+      </header>
+    </div>
+  );
+}
+
+export default App;
